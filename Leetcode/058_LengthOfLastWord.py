b3fb80b872f4ba216c344459a9c616f9af98973e
dionysus <dionysus.cho@gmail.com>
2017-09-28 22:37:43 +0900

    Leetcode 058 - Accepted 02
    cleaner (took out nested function) but no faster (10%)

--- Leetcode/058_LengthOfLastWord.py
+++ Leetcode/058_LengthOfLastWord.py
@@ -1,22 +1,33 @@
 def lengthOfLastWord(s):
 	
-	def length(n):
-		sum = 0
-
-		if len(n) < 1 or n[-1] == ' ':
-			return sum
-
-		if n[-1] != ' ':
-			sum += 1
+#sum = 0
 		
-		return sum + length(n[:-1])
-
+	#if len(s) < 1
+	#   return sum
+	
+	##1. Remove end _'s
+	#if s[-1] == ' '
+	#   loop: remove end _'s
+	
+	##2. Count length of remaining
+	#if s[] not empty
+	#   loop: count letters
+	#   if last != ' ', add += sum
+	
+	
 	if len(s) < 1:
-			return 0
-
+		return 0	# if s == ''
 	while s[-1] == ' ':
 		s = s[:-1]
+		if len(s) < 1:
+			return 0	
+	sum = 0
+	while len(s) > 0:
+		if s[-1] == ' ':
+			break
+		else:
+			s = s[:-1]
+			sum += 1
+	return sum
 
-	return length(s)
-
-print lengthOfLastWord('  ')
+print lengthOfLastWord('   ')
