3bc4832a705ac6071feec9c2e471851138c7906e
dionysus <dionysus.cho@gmail.com>
2017-09-28 16:00:56 +0900

    Leetcode 058 - Attempt02
    using recursive function instead of while loop

--- Leetcode/058_LengthOfLastWord.py
+++ Leetcode/058_LengthOfLastWord.py
@@ -1,11 +1,18 @@
 def lengthOfLastWord(s):
-
-	i = 0
 	
-	if len(s) > 0:
-		while s[-(i+1)] != ' ':
-			i += 1
-	
-	return i
+	sum = 0
 
+	if len(s) < 1:
+		return sum
+
+	if s[-1] != ' ':
+		sum += 1
+		return sum + lengthOfLastWord(s[:-1])
+	
+	return sum
+
+#n = ''
+#print n[-1]
 print lengthOfLastWord("")
+print lengthOfLastWord("a")
+print lengthOfLastWord("asdfs dfds")
