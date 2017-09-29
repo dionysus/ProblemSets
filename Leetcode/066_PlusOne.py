fff770879f3165abfebee9684897193e7885b66c
dionysus <dionysus.cho@gmail.com>
2017-09-29 11:40:46 +0900

    Leetcode 066 - Accepted
    Fast (77%!)

--- Leetcode/066_PlusOne.py
+++ Leetcode/066_PlusOne.py
@@ -4,26 +4,22 @@
 
 #The digits are stored such that the most significant digit is at the head of the list.
 
-class Solution(object):
-	def plusOne(self, digits):
-		"""
-		:type digits: List[int]
-		:rtype: List[int]
-		"""
-		
-		# if list is empty
-		# return none
+def plusOne(digits):
+	"""
+	:type digits: List[int]
+	:rtype: List[int]
+	"""
+	if len(digits) < 1:
+		return None
+	last = digits.pop()
 
-		# last = list.pop() # take off last number
+	if last != 9:
+		last += 1
+		return digits + [last]
+	else:
+		last = 0
+		if len(digits) == 0:
+			return [1] + [last]
+		return plusOne(digits) + [last]
 
-		# last += 1
-		# if last > 9
-		# last = 0
-
-		# num = plusOne(list) + last
-
-		# LOOP UNTIL DONE
-
-		# continue from right to left if 9
-		
-		# eg. 9799 --> 979[0] --> 97[0]0 --> 9[8]00
+#print plusOne([1,1,1,1,1,1,1,1,1,1,1,])
